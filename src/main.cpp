eaca54b6ff0ede24e0b5930a46eaf194acb880da
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2018-09-09 20:52:11 +0800

    add read rfid

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,52 @@
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include <SPI.h>
-#include <Wire.h>
-#include <FS.h>
 #include <ArduinoJson.h>
 #include <ArduinoLog.h>
+#include <ESP8266WiFi.h>
+#include <FS.h>
+#include <SPI.h>
+#include <Wire.h>
 
 #define DEBUG
 
 const char verTag[] = "beta 1.0";
 
+#include <LiquidCrystal_I2C.h>
 #include <MFRC522.h>
 #include "pitch.h"
-#include <LiquidCrystal_I2C.h>
 
 MFRC522 mfrc522 = MFRC522();
-LiquidCrystal_I2C lcd(0x3F,20,4);
+LiquidCrystal_I2C lcd(0x3F, 20, 4);
 
 bool isWifiConnected = false;
 bool inAPMode = false;
 unsigned long currentMillis = 0;
 unsigned long prevMillis = 0;
 unsigned long passMillis = 0;
+unsigned long cooldown = 0;
+
 
 #include "dogFiles/rfid.dog"
 #include "dogFiles/wifi.dog"
 #include "dogFiles/config.dog"
 
 void setup() {
-
     Serial.begin(9600);
-    #ifdef DEBUG
-        Log.begin   (LOG_LEVEL_VERBOSE, &Serial);
-    #else
-        Log.begin   (LOG_LEVEL_SILENT, &Serial);
-    #endif
+#ifdef DEBUG
+    Log.begin(LOG_LEVEL_VERBOSE, &Serial);
+#else
+    Log.begin(LOG_LEVEL_SILENT, &Serial);
+#endif
     Log.notice("[BOOT] wifi-watchdog %s\n", verTag);
 
     if (!SPIFFS.begin()) {
-		Log.warning("[SPIFFS] Formatting filesystem...");
-		if (SPIFFS.format()) {
+        Log.warning("[SPIFFS] Formatting filesystem...");
+        if (SPIFFS.format()) {
             Serial.println(F(" completed!"));
-		} else {
-			Serial.println(F(" failed!"));
-			Log.warning("[SPIFFS] Could not format filesystem!\n");
-		}
-	}
-
+        } else {
+            Serial.println(F(" failed!"));
+            Log.warning("[SPIFFS] Could not format filesystem!\n");
+        }
+    }
 
     loadConfig();
 }
@@ -54,4 +54,11 @@ void setup() {
 void loop() {
     currentMillis = millis();
     passMillis = currentMillis - prevMillis;
+    prevMillis = currentMillis;
+
+    if (currentMillis >= cooldown) {
+        rfidloop();
+    }
+    
+    
 }
